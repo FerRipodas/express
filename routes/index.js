ee414b08946094df0c32247878f957685043d5c9
FerRipodas <fernando.ripodas@gmail.com>
2022-06-16 21:52:06 -0300

    Buscador no terminado

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* GET nosotros page */
+router.get('/buscar', (req, res) => {
+
+
+  //guardar los que escribio el usuario
+//mostrarlo en la terminal
+
+//params y query
+//qury
+let { termino} = req.query;
+console.log(termino);
+
+  res.send('estas en buscar')
+});
+
+
+
+
 /* GET nosotros page */
 router.get('/nosotros', (req, res) => {
   res.render('pages/nosotros')
