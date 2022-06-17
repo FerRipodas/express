ce6990513ed8d5025b4f43470c9bebf218796798
FerRipodas <fernando.ripodas@gmail.com>
2022-06-16 21:23:40 -0300

    partials, routes y css

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET nosotros page */
+router.get('/nosotros', (req, res) => {
+  res.render('pages/nosotros')
+});
+
+/* GET nosotros page */
+router.get('/contacto', (req, res) => {
+  res.render('pages/contacto')
+});
+
+
+
 module.exports = router;
